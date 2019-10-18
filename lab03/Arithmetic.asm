70d980e93beeea611887b2c7fdb06c2c4bd36464
Frank Zhang <ruoqian@umail.ucsb.edu>
2019-10-18 12:03:58 -0700

    finished the funcs, not debug yet

--- lab03/Arithmetic.asm
+++ lab03/Arithmetic.asm
@@ -6,6 +6,24 @@
 main:
 
 	# TODO: Write your code here
+	li $v0, 5
+	syscall
+	move $t0, $v0
+	li $v0, 5
+	syscall
+	move $t1, $v1
+	li $v0, 5
+	syscall
+	move $t2, $v2
+	sub $t0, $t0, $t1
+	add $t0, $t0, $t0
+	mult $t2, 3
+	mflo $t1
+	add $a0, $t0, $t1
+	li $v0, 1
+	syscall
+	j exit
+	
 
 exit:
 	# Exit
--- lab03/hello.asm
+++ lab03/hello.asm
@@ -4,12 +4,42 @@
 #  Data Area - allocate and initialize variables
 .data
 	# TODO: Write your string definitions here
+	out_string:    .asciiz   "\nHello, World!\n"
+	ques:  		.asciiz  "Choose an integer number between 0 and 1000:\n"
+	User_chose: 	.asciiz  "User chose "
+	truly		.ssciiz  ". Truly a wise choice." 
+	
 
 #Text Area (i.e. instructions)
 .text
 main:
 
 	# TODO: Write your code here
+	li $v0, 4
+	la $a0, ques
+	syscall
+	li $v0, 4
+	syscall
+	li $a0, $v0
+	li $v0, 1
+	syscall
+	move $t0, $a0
+	li $v0, 4
+	la $a0, out_string
+	syscall
+	la $a0, User_chose
+	syscall
+	li $v0, 1
+	li $a0, $t0
+	syscall
+	la $a0, truly
+	li $v0, 4
+	syscall
+	j exit
+	
+	
+	
+	
 
 exit:
 	# Exit
